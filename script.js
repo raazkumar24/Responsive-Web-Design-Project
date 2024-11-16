43bc827a163206187181bbeffde5bc31ff010850
raazkumar24 <152789195+raazkumar24@users.noreply.github.com>
2024-11-16 18:32:48 +0530

    update

--- script.js
+++ script.js
@@ -12,14 +12,6 @@ close.addEventListener("click", () => {
   close.style.display = "none";
 });
 
-//active links
-// document.querySelectorAll('.nav-link').forEach(link => {
-//     link.addEventListener('click', function() {
-//       document.querySelectorAll('.nav-link').forEach(item => item.classList.remove('active'));
-//       this.classList.add('active');
-//     });
-//   });
-
 // Function to highlight the active link based on scroll position
 window.addEventListener("scroll", () => {
   let currentSection = "";
@@ -41,28 +33,5 @@ window.addEventListener("scroll", () => {
     if (link.getAttribute("href").substring(1) === currentSection) {
       link.classList.add("active");
     }
-  });
-});
-
-const projectRow = document.querySelector(".col");
-const dots = document.querySelectorAll(".dot");
-const projects = document.querySelectorAll(".project-box");
-const projectWidth = projects[0].offsetWidth + 20; // Project width + gap
-
-document.querySelector(".left-arrow").addEventListener("click", () => {
-  projectRow.scrollBy({ left: -projectWidth, behavior: "smooth" });
-});
-
-document.querySelector(".right-arrow").addEventListener("click", () => {
-  projectRow.scrollBy({ left: projectWidth, behavior: "smooth" });
-});
-
-// Update active dot based on the current scroll position
-projectRow.addEventListener("scroll", () => {
-  const scrollLeft = projectRow.scrollLeft;
-  const projectIndex = Math.round(scrollLeft / projectWidth);
-
-  dots.forEach((dot, index) => {
-    dot.classList.toggle("active", index === projectIndex);
   });
 });
